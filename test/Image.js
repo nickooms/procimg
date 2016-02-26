e838325c43bcd5a9c0e045adcb1aae75a2b8269d
Nick Ooms <oomsni@gmail.com>
2016-02-26 18:09:50 +0100

    var -> const

--- test/Image.js
+++ test/Image.js
@@ -1,82 +1,77 @@
 'use strict';
 
-var expect = require('chai').expect;
-var sinon = require('sinon');
-var Promise = require('es6-promise').Promise;
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const Promise = require('es6-promise').Promise;
 
 import Image from '../src/Image';
 
-var arnold = require('../src/arnold');
-
-console.log(Image);
+const arnold = require('../src/arnold');
 
 describe('Promises', function () {
 
-	before(function () {
-		console.log("It's showtime");
-	});
+	before(() => {});
 
-	it('object comparison', function () {
-		var user = { first: 'John', last: 'Matrix' };
-		var p = Promise.resolve(user);
+	it('object comparison', () => {
+		const user = { first: 'John', last: 'Matrix' };
+		const p = Promise.resolve(user);
 		return expect(p).to.become(user);
 	});
 
-	it('property comparison', function () {
-		var name = 'Kindergarten Cop';
-		var movie = { name: name, year: 1990 };
-		var p = Promise.resolve(movie);
+	it('property comparison', () => {
+		const name = 'Kindergarten Cop';
+		const movie = { name: name, year: 1990 };
+		const p = Promise.resolve(movie);
 		return expect(p.then(function (o) {
 			return o.name;
 		})).to.become(name);
 	});
 
-	it('multiple promise assertions', function () {
-		var msg1 = 'You are not you';
-		var msg2 = 'You are me';
-		var p1 = Promise.resolve(msg1);
-		var p2 = Promise.resolve(msg2);
+	it('multiple promise assertions', () => {
+		const msg1 = 'You are not you';
+		const msg2 = 'You are me';
+		const p1 = Promise.resolve(msg1);
+		const p2 = Promise.resolve(msg2);
 		return Promise.all([expect(p1).to.become(msg1), expect(p2).to.become(msg2)]);
 	});
 
-	it('comparing multiple promises', function () {
-		var p1 = Promise.resolve('Get up');
-		var p2 = Promise.resolve('Get down');
+	it('comparing multiple promises', () => {
+		const p1 = Promise.resolve('Get up');
+		const p2 = Promise.resolve('Get down');
 		return Promise.all([p1, p2]).then(function (values) {
 			expect(values[0]).to.not.equal(values[1]);
 		});
 	});
 
-	describe('rejected promises', function () {
+	describe('rejected promises', () => {
 		it('with a message', function () {
-			var message = 'Rubber baby buggy bumpers';
-			var p = Promise.reject(message);
+			const message = 'Rubber baby buggy bumpers';
+			const p = Promise.reject(message);
 			return expect(p).to.be.rejectedWith(message);
 		});
 
-		it('with a specific error type', function () {
-			var p = Promise.reject(new TypeError('Hey, christmas tree!'));
+		it('with a specific error type', () => {
+			const p = Promise.reject(new TypeError('Hey, christmas tree!'));
 			return expect(p).to.be.rejectedWith(TypeError);
 		});
 	});
 
-	describe('with stubs', function () {
-		it('return a resolved promise', function () {
-			//const message = 'Who is your daddy, and what does he do?';
-			var length = 983;
-			var stub = sinon.stub();
+	describe('with stubs', () => {
+		it('return a resolved promise', () => {
+			const filename = 'test/images/small.png';
+			const length = 983;
+			const stub = sinon.stub();
 			stub.resolves(length);
-			//const i = Image.load();
-			var buffer = Image.load('test/images/small.png'); //a.talkToTheHand();
+			const buffer = Image.load(filename);
 			return expect(buffer).to.become(length);
 		});
 
-		it('return a rejected promise', function () {
-			var message = "I'm the party pooper";
-			var stub = sinon.stub();
+		it('return a rejected promise', () => {
+			const message = "I'm the party pooper";
+			const stub = sinon.stub();
 			stub.rejects(message);
-			var a = arnold(stub);
-			var quote = a.talkToTheHand();
+			const a = arnold(stub);
+			const quote = a.talkToTheHand();
 			return expect(quote).to.be.rejectedWith(message);
 		});
 	});
