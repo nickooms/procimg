3abdca6ba00844b984df597bfdd7c47a52e7b9cf
Nick Ooms <oomsni@gmail.com>
2016-02-27 04:41:34 +0100

    Image.load

--- src/Image.js
+++ src/Image.js
@@ -5,15 +5,43 @@ import Canvas from 'canvas';
 
 class Image {
 
-	constructor() {
+	constructor(image) {
+		/*this.filename = image.filename;
+		this.filesize = image.filesize;
+		this.width = image.width;
+		this.height = image.height;
+		this.canvas = image.canvas;
+		this.context = image.context;*/
 	}
 
 	static load(filename) {
 		return new Promise((resolve, reject) => {
-			return fs.readFile(filename, function(err, data) {
-				if (err) reject(err);
+			return fs.readFile(filename, function(err, file) {
+				if (err) return reject(err);
+				const filesize = file.length;
+				const img = new Canvas.Image(file);
+				
+				
+				
+				
+				img.src = file;
+				//console.log(img.width);
+				const width = img.width;
+				const height = img.height;
+				const canvas = new Canvas(width, height);
+				const context = canvas.getContext('2d');
+				context.drawImage(img, 0, 0, width, height);
+				const imageData = context.getImageData(0, 0, width, height);
+				const data = new Uint32Array(imageData.data.buffer);
+				
 				//console.log(data.length);
-				resolve(data.length);
+
+				/*const canvas2 = new Canvas(w, h, 'svg');
+				const ctx2 = canvas2.getContext('2d');
+				ctx2.putImageData(imageData, 0, 0, w, h);
+				fs.writeFile('out.svg', canvas2.toBuffer());*/
+
+				resolve(filesize);
 			}); 
 		});
 	}
--- test/Image.js
+++ test/Image.js
@@ -8,17 +8,17 @@ import Image from '../src/Image';
 
 const arnold = require('../src/arnold');
 
-describe('Promises', function () {
+describe('Promises', function() {
 
 	before(() => {});
 
-	it('object comparison', () => {
+	it('object comparison', function() {
 		const user = { first: 'John', last: 'Matrix' };
 		const p = Promise.resolve(user);
 		return expect(p).to.become(user);
 	});
 
-	it('property comparison', () => {
+	it('property comparison', function() {
 		const name = 'Kindergarten Cop';
 		const movie = { name: name, year: 1990 };
 		const p = Promise.resolve(movie);
@@ -27,7 +27,7 @@ describe('Promises', function () {
 		})).to.become(name);
 	});
 
-	it('multiple promise assertions', () => {
+	it('multiple promise assertions', function() {
 		const msg1 = 'You are not you';
 		const msg2 = 'You are me';
 		const p1 = Promise.resolve(msg1);
@@ -35,7 +35,7 @@ describe('Promises', function () {
 		return Promise.all([expect(p1).to.become(msg1), expect(p2).to.become(msg2)]);
 	});
 
-	it('comparing multiple promises', () => {
+	it('comparing multiple promises', function() {
 		const p1 = Promise.resolve('Get up');
 		const p2 = Promise.resolve('Get down');
 		return Promise.all([p1, p2]).then(function (values) {
@@ -43,62 +43,35 @@ describe('Promises', function () {
 		});
 	});
 
-	describe('rejected promises', () => {
+	describe('rejected promises', function() {
 		it('with a message', function () {
 			const message = 'Rubber baby buggy bumpers';
 			const p = Promise.reject(message);
 			return expect(p).to.be.rejectedWith(message);
 		});
 
-		it('with a specific error type', () => {
+		it('with a specific error type', function() {
 			const p = Promise.reject(new TypeError('Hey, christmas tree!'));
 			return expect(p).to.be.rejectedWith(TypeError);
 		});
 	});
 
-	describe('with stubs', () => {
-		it('return a resolved promise', () => {
+	describe('Image.load', function() {
+		it('return a resolved promise', function() {
 			const filename = 'test/images/small.png';
-			const length = 983;
+			const length = 1051;
 			const stub = sinon.stub();
 			stub.resolves(length);
 			const buffer = Image.load(filename);
 			return expect(buffer).to.become(length);
 		});
 
-		it('return a rejected promise', () => {
-			const message = "I'm the party pooper";
+		it('return a rejected promise', function() {
+			const filename = 'test/images/unknown.png';
 			const stub = sinon.stub();
-			stub.rejects(message);
-			const a = arnold(stub);
-			const quote = a.talkToTheHand();
-			return expect(quote).to.be.rejectedWith(message);
+			stub.rejects(filename);
+			const buffer = Image.load(filename);
+			return expect(buffer).to.be.rejectedWith('no such file or directory');
 		});
 	});
 });
-
-/*'use strict';
-
-import { createGroup, assert } from 'painless';
-import Image from '../src/Image';
-
-const test = createGroup();
-
-test('sync test', () => {
-  assert.deepEqual({ a: '1' }, { a: '1' });
-});
-
-test('promise test', () => {
-	const FILENAME = `test/images/small.png`;
-	//const FILENAME = `test/images/GRB_WGO__152550.96,221839.05,152582.63,221870.31.png`;
-	//console.log(Image);
-	//for (let a in Image) console.log(a);
-	let promise = Image.load(FILENAME);
-	promise.then(f => {
-		console.log(a);
-	})
-	//console.log(promise);
-
-  return assert.eventually.deepEqual(promise.length, 983);
-});
-*/
